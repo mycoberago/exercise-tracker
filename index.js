b0d4f8e62b8b355b75d88310c49e03d6af9c9b1b
mycomike <mycoberago@gmail.com>
2023-11-30 10:25:24 -0600

    final accepter solution

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 app.use(express.static('public'))
 
-mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
@@ -23,28 +23,28 @@ const usersSchema = new mongoose.Schema({
 })
 
 const exerciseSchema = new mongoose.Schema({
-    date: {
-      type: Date
-    },
-    duration: {
-      type: Number
-    },
-    description: {
-      type: String
-    },
-    id: {
-      type: ObjectId
-    }
+  date: {
+    type: Date
+  },
+  duration: {
+    type: Number
+  },
+  description: {
+    type: String
+  },
+  id: {
+    type: ObjectId
+  }
 })
 
-let User = mongoose.model('User',usersSchema);
-let Exercise = mongoose.model('Exercise',exerciseSchema);
+let User = mongoose.model('User', usersSchema);
+let Exercise = mongoose.model('Exercise', exerciseSchema);
 
 //  CREATE USER
-app.post('/api/users',async (req,res) => {
+app.post('/api/users', async (req, res) => {
   let { username } = req.body;
-  if(!username) {
-    return res.json({error: 'no username'})
+  if (!username) {
+    return res.json({ error: 'no username' })
   }
   let user = new User({ username: username });
 
@@ -57,162 +57,196 @@ app.post('/api/users',async (req,res) => {
 
 })
 
-app.post('/api/users/:_id/exercises', async (req,res) => {
-  let { _id, description, duration, date } = req.body;
-  if(!date) {
-    date = new Date().toDateString();
-  } else {
-    date = new Date(date).toDateString();
-  }
+app.post('/api/users/:_id/exercises', async (req, res) => {
+  console.log(req.params);
+  let _id = req.params._id;
+  let description = req.body.description;
+  let duration = parseInt(req.body.duration);
+  let date = req.body.date ? new Date(req.body.date).toDateString() : new Date().toDateString();
 
   try {
     let user = await User.findById(_id);
 
-    let doc = {
-      id: _id,
-      description:description,
-      duration: duration,
-      date:date
-    }
+    if (user) {
 
-    let exercise = new Exercise(doc);
+      let doc = {
+        id: _id,
+        description: description,
+        duration: duration,
+        date: date
+      }
 
-    await exercise.save();
+      let exercise = new Exercise(doc);
 
-    res.json({
-      _id: _id,
-      username: user.username,
-      description:description,
-      duration: duration,
-      date:date
-    });
+      await exercise.save();
+
+      return res.json({
+        _id: user._id,
+        username: user.username,
+        date: date,
+        duration: duration,
+        description: description
+      });
+
+    } //  IF THERE IS A USER;
+
+    return res.json({ error: 'Unable to find user' })
 
   } catch (error) {
-    res.json({error: error.message})
+    return res.json({ error: error.message })
   }
-  
+
 })
 
 // create user
-app.get('/api/users', async (req,res) => {
+app.get('/api/users', async (req, res) => {
   let users = await User.find();
   res.json(users);
 })
 
 //  GET LOGS
-app.get('/api/users/:_id/logs', async (req,res) => {
+app.get('/api/users/:_id/logs', async (req, res) => {
   let { _id } = req.params;
   let { query } = req;
   console.log(query);
-  let from,to;
+  let from, to;
   let limit = 0;
 
   //  If there are query parameters
-  if(Object.keys(query).length > 0) {
+  if (Object.keys(query).length > 0) {
     from = query?.from;
     to = query?.to;
     limit = query?.limit ? parseInt(query?.limit) : 0;
   }
 
-  if(from && to && limit > 0) {
+  if (from && to && limit > 0) {
 
-    let data = await User.aggregate([
-      { $match: { _id: new mongoose.Types.ObjectId(_id)}},
-      {
-        $lookup: {
-          from: 'exercises',
-          localField: '_id',
-          foreignField: 'id',
-          as: 'log',
-          pipeline:[
-            {
-            $match:{
-              date: {
-                $gt: new Date(from),
-                $lt: new Date(to)
-              }
-            }    
-          },
-          { $limit: limit }
-        ]
-        }
-      }
-    ])
-
-    if(data.length > 0) {
-      let result = data[0];
-      result['count'] = result.log.length;
-      return res.json(result);
-    }
-  
-    return res.send("No data found");
-
-  }
-
-  //  If there is no limit
-  if(from && to) {
-
-    let data = await User.aggregate([
-      { $match: { _id: new mongoose.Types.ObjectId(_id)}},
-      {
-        $lookup: {
-          from: 'exercises',
-          localField: '_id',
-          foreignField: 'id',
-          as: 'log',
-          pipeline:[
-            {
-            $match:{
-              date: {
-                $gt: new Date(from),
-                $lt: new Date(to)
-              }
-            }    
+    try {
+      let data = await User.aggregate([
+        { $match: { _id: new mongoose.Types.ObjectId(_id) } },
+        {
+          $lookup: {
+            from: 'exercises',
+            localField: '_id',
+            foreignField: 'id',
+            as: 'log',
+            pipeline: [
+              {
+                $match: {
+                  date: {
+                    $gt: new Date(from),
+                    $lt: new Date(to)
+                  }
+                }
+              },
+              { $limit: limit }
+            ]
           }
-        ]
         }
-      }
-    ])
+      ])
 
-    if(data.length > 0) {
-      let result = data[0];
-      result['count'] = result.log.length;
-      return res.json(result);
+      if (data.length > 0) {
+        let result = data[0];
+        result['count'] = result.log.length;
+        for(var i of result['log']) {
+          i['date'] = new Date(i['date']).toDateString();
+        }
+        console.log(result);
+        return res.json(result);
+      } //  IF DATA
+      return res.json({ error: 'Unable to find user' });
+      
+    } catch (error) {
+      res.json({ error: error.message })
     }
-  
-    return res.send("No data found");
 
-  }
 
-  if(limit > 0) {
+  } //  LIMIT and RANGE
 
+//  If there is no limit
+if (from && to) {
+
+  try {
     let data = await User.aggregate([
-      { $match: { _id: new mongoose.Types.ObjectId(_id)}},
+      { $match: { _id: new mongoose.Types.ObjectId(_id) } },
       {
         $lookup: {
           from: 'exercises',
           localField: '_id',
           foreignField: 'id',
           as: 'log',
-          pipeline:[
+          pipeline: [
+            {
+              $match: {
+                date: {
+                  $gt: new Date(from),
+                  $lt: new Date(to)
+                }
+              }
+            }
+          ]
+        }
+      }
+    ])
+
+    if (data.length > 0) {
+      let result = data[0];
+      result['count'] = result.log.length;
+      for(var i of result['log']) {
+        i['date'] = new Date(i['date']).toDateString();
+      }
+      console.log(result);
+      return res.json(result);
+    }
+    return res.send("No data found");
+  } catch (error) {
+    return res.json({ error: error.message })
+  }
+
+}  // NO LIMIT BUT THERE IS RANGE
+
+if (limit > 0) {
+
+  try {
+
+    let data = await User.aggregate([
+      { $match: { _id: new mongoose.Types.ObjectId(_id) } },
+      {
+        $lookup: {
+          from: 'exercises',
+          localField: '_id',
+          foreignField: 'id',
+          as: 'log',
+          pipeline: [
             { $limit: limit }
           ]
         }
       }
     ])
 
-    if(data.length > 0) {
+    if (data.length > 0) {
       let result = data[0];
       result['count'] = result.log.length;
+      for(var i of result['log']) {
+        i['date'] = new Date(i['date']).toDateString();
+      }
+      console.log(result);
       return res.json(result);
     }
-  
+
     return res.send("No data found");
+
+  } catch (error) {
+
+    return res.json({ error: error.message })
 
   }
 
+} //  LIMIT ONLY
+
+try {
   const defaultSearch = await User.aggregate([
-    { $match: { _id: new mongoose.Types.ObjectId(_id)}},
+    { $match: { _id: new mongoose.Types.ObjectId(_id) } },
     {
       $lookup: {
         from: 'exercises',
@@ -223,53 +257,21 @@ app.get('/api/users/:_id/logs', async (req,res) => {
     }
   ])
 
-  if(defaultSearch.length > 0) {
+  if (defaultSearch.length > 0) {
     let result = defaultSearch[0];
     result['count'] = result.log.length;
+    for(var i of result['log']) {
+      i['date'] = new Date(i['date']).toDateString();
+    }
+    console.log(result);
     return res.json(result);
   }
 
   return res.send("No data found");
 
-  
-
-  
-
-  // User.aggregate().match({_id: _id}).lookup({
-  //   from: 'exercise',
-  //   localField: 'id', foreignField: '_id',
-  //   as: 'log'
-  // }).exec()
-  // .then((results) => {
-  //   console.log(results);
-  //   res.send(JSON.stringify(results))
-  // })
-  // .catch(error => {
-  //   res.json({error: error.message})
-  // })
-
-  // console.log('logs');
-
-  // //  If there are query parameters
-  // if(Object.keys(query).length > 0) {
-  //   from = query?.from;
-  //   to = query?.to;
-  //   limit = query?.limit || 0;
-  // }
-
-  // if(from && to) {
-
-  //   let exercise = await User.find({_id: _id,"log.date":{$gt: from, $lt: to }});
-
-  //   return res.json(exercise);
-  // }
-
-  // // let user = await User.find()
-  // res.json(user);
-})
-
-app.get('/test', async (req,send) => {
-  res.send('hello world');
+} catch (error) {
+  return res.json({ error: error.message })
+}
 })
 
 
